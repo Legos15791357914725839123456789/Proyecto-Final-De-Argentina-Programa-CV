fda0fa2b000cc11bc38290df871d75a03b4acd1b
Ryu_Aoyama <legostoppy.00@gmail.com>
2023-02-28 20:29:22 -0300

    Pagina Completada

--- Proyecto-Final-de-Argentina-Programa.js
+++ Proyecto-Final-de-Argentina-Programa.js
@@ -1,7 +1,12 @@
+document.getElementById("botonmodoclaro").onclick = function () {
+    document.body.style.backgroundColor =  'white'
+    document.body.style.color = 'black'
 
+};
 
 
 document.getElementById("botonmodoobscuro").onclick = function () {
     document.body.style.backgroundColor =  'black'
     document.body.style.color = 'white'
+
 };
